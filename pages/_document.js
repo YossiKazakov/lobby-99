38e0162e2eed381fedb5ef6de69b89043c94b1b6
OmriO19 <omri19995@gmail.com>
2023-01-03 13:20:46 +0200

    tryin to fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
     <Html>
       <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Arimo&family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
       </Head>
       <body>
