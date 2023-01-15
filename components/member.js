40294c9bbfce194cedb74cddfb0da353af1c2e40
Yossi Kazakov <yossikazakov1@gmail.com>
2023-01-15 10:15:06 +0200

    member, memberstyle,opening style and utils STASH

--- components/member.js
+++ components/member.js
@@ -11,6 +11,7 @@ import Past from './past';
 import Future from './future';
 import Connector from './theconnector';
 import NextVote from './next_vote_frame';
+import LobbyEmpire from './lobby_empire';
 
 import turn from '../images/turn.svg'
 import equal from '../images/equal.svg'
@@ -18,7 +19,61 @@ import Frame3 from './frame_3';
 import React, { useEffect, useRef } from 'react';
 
 
-export default function Member({ member }) {
+// export default function Member({ member }) {
+//     const openingOneRef = useRef(null) // The following is responsible for the auto scroll
+//     const openingTwoRef = useRef(null)
+//     useEffect(() => {
+//         setTimeout(() => {
+//             openingTwoRef.current.scrollIntoView({ behavior: 'smooth' });
+//         }, 3000);
+//     }, []);
+//     return (
+//         <div className={styles.container}>
+//             <div ref={openingOneRef} className={styles.frame}>
+//                 <OpeningOne />
+//             </div>
+//             <div ref={openingTwoRef} className={styles.frame}>
+//                 <OpeningTwo />
+//             </div>
+//             <div className={styles.frame}>
+//                 <ThankYou />
+//             </div>
+//             <div className={styles.frame}>
+//                 <Name name={member.name} />
+//             </div>
+//             <div className={styles.frame}>
+//                 <Unique />
+//             </div>
+//             <div className={styles.frame}>
+//                 <Past />
+//             </div>
+
+//             {/* in the meantime just photos */}
+//             <div className={styles.frame}>
+//                 <Connector />
+//             </div>
+//             <div className={styles.frame}>
+//                 <Frame3 image={
+//                     <Image id='equal' src={equal} alt="all voices are equal" fill />
+//                 } />
+//             </div>
+//             <div className={styles.frame}>
+//                 <Frame3 image={
+//                     <Image id='turn' src={turn} alt="now it's your turn" fill />
+//                 } />
+//             </div>
+
+//             <div className={styles.frame}>
+//                 <Future />
+//             </div>
+
+//             <div className={styles.frame}>
+//                 <NextVote name={member.name} />
+//             </div>
+//         </div>
+//     )
+// }
+export default function Member({ member , numOfMembers}) {
     const openingOneRef = useRef(null) // The following is responsible for the auto scroll
     const openingTwoRef = useRef(null)
     useEffect(() => {
@@ -37,35 +92,9 @@ export default function Member({ member }) {
             <div className={styles.frame}>
                 <ThankYou />
             </div>
-            <div className={styles.frame}>
-                <Name name={member.name} />
-            </div>
-            <div className={styles.frame}>
-                <Unique />
-            </div>
-            <div className={styles.frame}>
-                <Past />
-            </div>
-
-            {/* in the meantime just photos */}
-            <div className={styles.frame}>
-                <Connector />
-            </div>
-            <div className={styles.frame}>
-                <Frame3 image={
-                    <Image id='equal' src={equal} alt="all voices are equal" fill />
-                } />
-            </div>
-            <div className={styles.frame}>
-                <Frame3 image={
-                    <Image id='turn' src={turn} alt="now it's your turn" fill />
-                } />
-            </div>
-
-            <div className={styles.frame}>
-                <Future />
-            </div>
-
+           <div className={styles.frame}>
+                <LobbyEmpire />
+           </div>
             <div className={styles.frame}>
                 <NextVote name={member.name} />
             </div>
--- utils.js
+++ utils.js
@@ -1,17 +1,25 @@
-export function parseData({ data }){
-    console.log(data);
+import { MEMBERS_TABLE_NAME } from "./config";
+import { client } from "./database";
+
+export function parseData({ data }) {
+    // console.log(data);
     let members = []
     data.map((e) => {
         const member = {
-        id: e.id,
-        name: e.name};
-    members.push(member);
+            id: e.id,
+            name: e.name
+        };
+        members.push(member);
     });
     return members;
 }
 
-export function calculateTime({ join_at })
-{
-
+export function calculateTime({ join_at }) {
     return years, months, days;
 }
+
+// export async function numberOfMembers() {
+//     const { data } = await client.from(MEMBERS_TABLE_NAME).select('*')
+//     // return Object.keys(data).map(key => data[key]).length;
+//     return data.length
+// }
