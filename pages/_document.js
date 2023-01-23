1d99ea30dda77d6fbe51abf15519d66f39045e55
OmriO19 <omri19995@gmail.com>
2023-01-23 13:56:26 +0200

    .....

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="he_IL" />
         <meta name="theme-color" content="#FFC4B5" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="#FFC4B5">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#FFC4B5" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="display" content="fullscreen" />
