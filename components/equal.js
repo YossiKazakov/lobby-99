97e7c06ef7a697adca2ea9c3cc3ae216954d81c5
Yossi Kazakov <yossikazakov1@gmail.com>
2023-07-03 11:41:08 +0300

    small fix

--- components/equal.js
+++ components/equal.js
@@ -9,23 +9,24 @@ import styles from '../styles/equal.module.css';
 // This frame shows a message that evry vote is equal
 
 export default function Equal() {
-    const { ref: ref, inView: elementIsVisible } = useInView({triggerOnce: true});
+    const { ref: ref, inView: elementIsVisible } = useInView({ triggerOnce: false });
 
     return (
         <div className={styles.container}>
             <div ref={ref} className={styles.text1}>
-                זה לא משנה 
-                <br/>
-                אם הגעת אלינו 
-                <br/>
-                לא מזמן 
-                <span style={{color:'#6B53FF'}}> או</span>
-                <br/>
+                זה לא משנה
+                <br />
+                אם הגעת אלינו
+                <br />
+                לא מזמן
+                <span style={{ color: '#6B53FF' }}> או</span>
+                <br />
                 איתנו כבר שנים
             </div>
 
             <div className={`${styles.main} ${elementIsVisible ? styles.spin : ''}`}>
-                    <Image id='main' src={equalStar} alt="star" fill/>
+                <Image id='main' src={equalStar} alt="star" fill />
             </div>
         </div>
-        )}
+    )
+}
